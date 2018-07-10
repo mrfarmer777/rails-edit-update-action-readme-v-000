4a6af832935f75f0262fc2f6b0408a81986ce88a
Matt Farmer <mrfarmer777@gmail.com>
2018-07-10 17:40:04 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,5 +28,6 @@ class PostsController < ApplicationController
 		raise params.inspect
 		@post=Post.find(params[:id])
 		@post.update(title:params[:title],description:params[:description])
+		redirect_to post_path(@post)
 	end
 end
